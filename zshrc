79550bb3f91064a0acc5cdd02c054a9a0dd1f87c
Ulrik Lejon <ulrik.lejon@consultant.volvofinans.se>
2021-04-13 08:20:08 +0200

    Updates

--- zshrc
+++ zshrc
@@ -110,5 +110,12 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
+NPM_PACKAGES="${HOME}/.npm-packages"
+PATH="$NPM_PACKAGES/bin:$PATH"
+
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+unset MANPATH # delete if you already modified MANPATH elsewhere in your config
+export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- zshrccustom/1.zsh
+++ zshrccustom/1.zsh
@@ -1,9 +1,15 @@
+#export DISPLAY=127.0.0.1:0.0
+#sudo /etc/init.d/dbus start &> /dev/null
+
+export NVM_DIR="$HOME/.nvm"
+alias loadnvm='[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"'
+
 export LS_COLORS="$LS_COLORS:ow=1;34:tw=1;34:"
 zstyle ':completion:*' list-colors
 
 alias ll='ls -al'
 
-export DOCKER_HOST=tcp://0.0.0.0:2375
+export DOCKER_HOST=tcp://localhost:2375
 export VOLVOFINANS_USERNAME=dfc1012
 export VOLVOFINANS_X_USERNAME=dfcx1012
 export JAVA_HOME=/opt/java
@@ -21,3 +27,4 @@ alias vf-crypto-256="/opt/volvofinans/wsl/bin/vf-crypto-256"
 alias vault-certificates="/opt/volvofinans/wsl/bin/vault-certificates"
 alias vault-ldap-login="/opt/volvofinans/wsl/bin/vault-ldap-login"
 alias tag-kong-portal-configuration="/opt/volvofinans/wsl/bin/tag-kong-portal-configuration"
+alias cls='printf "\033c"'
